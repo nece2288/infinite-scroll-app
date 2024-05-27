91e928e5a60437d42dc1ea5e04d1eefb3a7a9662
Simonas Snarskas <simonas@Simonass-MBP.lan>
2024-05-27 21:34:59 +0300

    cleaned up unecessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import getCats from "./utils/getCats";
